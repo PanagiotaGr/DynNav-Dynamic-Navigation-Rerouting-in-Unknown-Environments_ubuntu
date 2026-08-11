67807c16074b49f007cfb498e1baf9a7435ad224
panagiotagrosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-08-11 20:13:57 +0300

    Add TurtleBot3 integration python setup

---
+++ ros2_ws/src/dynnav_turtlebot3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+package_name = 'dynnav_turtlebot3'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[],
+    data_files=[],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    description='DynNav TurtleBot3 Gazebo integration',
+)
